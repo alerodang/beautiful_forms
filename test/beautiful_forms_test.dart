bef05ef2ba81de52e914c246b644b4515f96e6de
ale <alerodriguezangulo@gmail.com>
2020-12-06 13:40:05 +0000

    Add example

--- test/beautiful_forms_test.dart
+++ test/beautiful_forms_test.dart
@@ -3,11 +3,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:beautiful_forms/builder/form_builder.dart';
 
 void main() {
-//  test('adds one to input values', () {
-//    final calculator = BeautifullForm();
-//    expect(calculator.addOne(2), 3);
-//    expect(calculator.addOne(-7), -6);
-//    expect(calculator.addOne(0), 1);
-//    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-//  });
+
 }
