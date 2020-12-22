b004bdf2ecbaa5bab96e53779c218b70c8e994fe
ale <alerodriguezangulo@gmail.com>
2020-12-22 17:00:55 +0000

    UNDO register

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -1,13 +1,13 @@
 //
 // Generated file. Do not edit.
 //
-
-import 'package:url_launcher_web/url_launcher_web.dart';
-
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-
-// ignore: public_member_api_docs
-void registerPlugins(Registrar registrar) {
-  UrlLauncherPlugin.registerWith(registrar);
-  registrar.registerMessageHandler();
-}
+//
+//import 'package:url_launcher_web/url_launcher_web.dart';
+//
+//import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+//
+//// ignore: public_member_api_docs
+//void registerPlugins(Registrar registrar) {
+//  UrlLauncherPlugin.registerWith(registrar);
+//  registrar.registerMessageHandler();
+//}
